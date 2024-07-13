5360058d3de78ba324a470341a2b3f0e5e162251
DrvxTheCoder <princeflan741@gmail.com>
2024-07-13 01:50:59 +0000

    chore: Further UI tweaks

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -20,7 +20,7 @@ export default function AuthenticationPage() {
     <>
     <title>ToubaApp™ - Appli Officiel de Touba Oil SAU</title>
       <div className="container relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        <div className="relative hidden h-full flex-col p-10 text-white dark:border-r lg:flex">
+        <div className="relative hidden h-full flex-col p-6 text-white dark:border-r lg:flex">
           <div className="absolute inset-0 bg-zinc-900 opacity-70" />
           <Image
             src="/assets/img/touba-cover.jpeg"
@@ -43,18 +43,18 @@ export default function AuthenticationPage() {
           </div>
         </div>
         <div className=" mt-36 justify-center lg:mt-12">
-          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[30rem] border rounded-lg shadow-lg p-10">
-            <div className="flex flex-col space-y-2 text-center">
+          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[30rem] border rounded-lg shadow-lg p-6">
+            <div className="flex flex-col space-y-1 text-center">
               
-              <h1 className="text-4xl font-bold tracking-tight">
+              <h1 className="text-3xl font-bold tracking-tight">
                 Connexion
               </h1>
-              <p className="white text-sm text-muted-foreground">
-              Entrez vos identifiants pour accéder à votre compte.
+              <p className="white text-xs text-muted-foreground">
+              Connexion à l&apos;appli ToubaApp.
               </p>
             </div>
             <UserAuthForm />
-            <small className="px-8 text-xs text-center text-muted-foreground">
+            <small className="px-6 text-xs text-center text-muted-foreground">
             En cliquant sur Continuer, vous acceptez nos{" "}
               <Link
                 href="/terms"
